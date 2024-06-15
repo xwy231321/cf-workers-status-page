004905d0f5497a3001d5d84fc0fc037e3bc39356
xwy <gudao615970148@163.com>
2024-06-15 23:40:46 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -89,22 +89,13 @@ export default function Index({ config, kvMonitors, kvMonitorsLastUpdate }) {
           )
         })}
         <div className="flex flex-row justify-between mt-4 text-sm">
-          <div>
-            Powered by{' '}
-            <a href="https://workers.cloudflare.com/" target="_blank">
-              Cloudflare Workers{' '}
-            </a>
-            &{' '}
-            <a href="https://flareact.com/" target="_blank">
-              Flareact{' '}
-            </a>
-          </div>
+          
           <div>
             <a
-              href="https://github.com/eidam/cf-workers-status-page"
+              href="https://xwy231321.top"
               target="_blank"
             >
-              Get Your Status Page
+              Go to alist
             </a>
           </div>
         </div>
